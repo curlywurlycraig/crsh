36cdb9f17c11fb7bb03ca368eb1626b188e60c1f
Craig Wilkinson <curlywurlycraig@gmail.com>
2020-07-12 09:13:29 +0100

    Add default yarn commands

--- completionRules/npm.js
+++ completionRules/npm.js
@@ -1,14 +1,63 @@
 import { exec } from "../util.js";
 
+const yarnCommands = [
+  "access",
+  "add",
+  "audit",
+  "autoclean",
+  "bin",
+  "cache",
+  "check",
+  "config",
+  "create",
+  "exec",
+  "generateLockEntry",
+  "global",
+  "help",
+  "import",
+  "info",
+  "init",
+  "install",
+  "licenses",
+  "link",
+  "list",
+  "login",
+  "logout",
+  "node",
+  "outdated",
+  "owner",
+  "pack",
+  "policies",
+  "publish",
+  "remove",
+  "run",
+  "tag",
+  "team",
+  "unlink",
+  "unplug",
+  "upgrade",
+  "upgradeInteractive",
+  "version",
+  "versions",
+  "why",
+  "workspace",
+  "workspaces",
+];
+
 // TODO Make work with yarn AND npm
 const npmRules = [
   {
     match: /^yarn /,
     complete: async (token, tabIndex) => {
       const packageJsonRaw = await exec("cat", ["package.json"]);
-      const commands = Object.keys(
-        JSON.parse(packageJsonRaw).scripts
-      ).filter((command) => command.startsWith(token));
+      const customCommands =
+        packageJsonRaw.length > 0
+          ? Object.keys(JSON.parse(packageJsonRaw).scripts)
+          : [];
+
+      const commands = [...customCommands, ...yarnCommands].filter((command) =>
+        command.startsWith(token)
+      );
 
       return commands[tabIndex % commands.length];
     },
